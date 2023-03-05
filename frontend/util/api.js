1f3455e8f06417c2c44bf794ddfc9d5966df0a39
0xdagarn <dagarn@unopnd.com>
2023-03-06 00:15:13 +0900

    Use https

--- frontend/util/api.js
+++ frontend/util/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://ec2-43-201-71-58.ap-northeast-2.compute.amazonaws.com:3000",
+  baseURL: "https://ec2-43-201-71-58.ap-northeast-2.compute.amazonaws.com:3000",
 });
 
 export default api;
